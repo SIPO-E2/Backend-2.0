7337ce9ad35cdcf7d067dc34328032926f19abdc
danielagallardoc <a01252869@tec.mx>
2024-03-17 11:51:22 -0700

    config

--- src/db/config.ts
+++ src/db/config.ts
@@ -2,15 +2,20 @@ import { Sequelize } from 'sequelize-typescript';
 import {User} from '../models/user';
 import {Client} from '../models/client';
 import { Employee } from '../models/employee';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Daniela
+//  daniela Abelanda22
 
 const connection = new Sequelize({
-  database: "sipo",
-  username: "daniela",
-  password: "Abelenda22",
-  host: "localhost",
-  port: 5432,
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   dialect: 'postgres',
-  models: [User, Client,Employee],
+  models: [User, Client, Employee],
   storage: ':memory:',
 });
 
