c7202217b5f00065655e35b3790439c6321e2751
danielagallardoc <a01252869@tec.mx>
2024-05-02 18:55:58 -0700

    changes a main

--- src/models/project.ts
+++ src/models/project.ts
@@ -3,7 +3,6 @@ import {
   Column,
   Model,
   DataType,
-  AllowNull,
   CreatedAt,
   UpdatedAt,
   DeletedAt,
@@ -17,8 +16,7 @@ import { User } from "./user";
 import { JobPosition } from "./jobPosition";
 import { Status, Region } from "./enums";
 
-
-interface ProjectAttributes{
+interface ProjectAttributes {
     id: number;
     owner_user_id: number;
     owner_user: User;
@@ -28,7 +26,7 @@ interface ProjectAttributes{
     status: Status;
     reason_current_status: string;
     status_date: Date;
-    progress: Date;
+    progress: number;
     revenue: number;
     region: Region;
     posting_date: Date;
@@ -37,23 +35,20 @@ interface ProjectAttributes{
     job_positions_list: JobPosition[];
     activeDB: boolean;
 }
-// Optional id, revenue and activeDB
-export interface ProjectCreationAttributes extends Optional<ProjectAttributes, 'id' |'progress'| 'status_date' | 'revenue' | 'activeDB' | "owner_user" | "owner_client" | "job_positions_list"> {}
+
+export interface ProjectCreationAttributes extends Optional<ProjectAttributes, 'id' | 'progress' | 'status_date' | 'revenue' | 'activeDB' | 'owner_user' | 'owner_client' | 'job_positions_list'> {}
 
 @Table({
   tableName: "project",
   timestamps: true,
   paranoid: true,
 })
-export class Project extends Model<
-  ProjectAttributes,
-  ProjectCreationAttributes
-> {
+export class Project extends Model<ProjectAttributes, ProjectCreationAttributes> {
   @Column(DataType.STRING)
   public name!: string;
 
   @Column(DataType.ENUM(...Object.values(Status)))
-  public status!: Region;
+  public status!: Status;
 
   @Column(DataType.STRING)
   public reason_current_status!: string;
@@ -88,30 +83,23 @@ export class Project extends Model<
   @Column
   public deletedAt!: Date;
 
-  // Default true
-  @Column({ type:DataType.BOOLEAN, defaultValue: true })
+  @Column({ type: DataType.BOOLEAN, defaultValue: true })
   public activeDB!: boolean;
 
-  // Foreign key user
   @ForeignKey(() => User)
   @Column(DataType.INTEGER)
   public owner_user_id!: number;
 
-  //Has one User
   @BelongsTo(() => User)
   public owner_user!: User;
 
-  // Foreign key client
   @ForeignKey(() => Client)
   @Column(DataType.INTEGER)
   public owner_client_id!: number;
 
-  //Has one Client
   @BelongsTo(() => Client)
   public owner_client!: Client;
 
-  //Has many job_positions
   @HasMany(() => JobPosition)
   public job_positions_list!: JobPosition[];
-    
 }
