807b5382a6572194c09a9322670b3b55217c06bd
umakossiooo <a01254619@tec.mx>
2024-05-03 03:01:28 -0700

    build: created restrictions for openings controllers

--- src/controllers/openings.ts
+++ src/controllers/openings.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { Employee, Project, User } from '../models';
 import { JobPosition } from '../models';
 import { Opening } from '../models';
-import {OpeningCreationAttributes} from '../models/opening';
+import { OpeningCreationAttributes } from '../models/opening';
 
 
 // Getting openings
@@ -10,7 +10,7 @@ export const getOpenings = async (req: Request, res: Response) => {
   const { from = 0, to = 5 } = req.query;
 
   // DB
-  await Opening.findAll({ offset: Number(from), limit: Number(to), include: [{ model: JobPosition, as: "owner_jobPosition", include:[{model:Project, as: "owner_project", include:[{model:User, as: "owner_user"}]}] }] })
+  await Opening.findAll({ offset: Number(from), limit: Number(to), include: [{ model: JobPosition, as: "owner_jobPosition", include: [{ model: Project, as: "owner_project", include: [{ model: User, as: "owner_user" }] }] }] })
     .then((openings) => {
       res.json({
         status: "success",
@@ -31,8 +31,7 @@ export const getOpenings = async (req: Request, res: Response) => {
 export const getOpening = async (req: Request, res: Response) => {
   const { id } = req.params;
 
-  // DB
-  await Opening.findByPk(id, { include: [{ model: JobPosition, as: "owner_jobPosition", include:[{model:Project, as: "owner_project", include:[{model:User, as: "owner_user"}]}] }] })
+  await Opening.findByPk(id, { include: [{ model: JobPosition, as: "owner_jobPosition", include: [{ model: Project, as: "owner_project", include: [{ model: User, as: "owner_user" }] }] }] })
     .then((openings) => {
       res.json({
         status: "success",
@@ -50,65 +49,80 @@ export const getOpening = async (req: Request, res: Response) => {
 };
 
 // Creating a opening
-export const createOpening = async(req: Request, res: Response) => {
-    const { status, reason_current_status, open_date, close_date, close_reason, hours_required, owner_jobPosition_id }:OpeningCreationAttributes = req.body;
-    
-    // // if employee not found return error because the relationship is required
-    // const employee = await Employee.findByPk(employee_id);
-    // if (!employee) {
-    //     res.json({
-    //         status: "error",
-    //         message: " Employee of Opening not found",
-    //     });
-    //     return;
-    // }
+export const postOpening = async (req: Request, res: Response) => {
+  const { status, reason_current_status, open_date, close_date, close_reason, hours_required, owner_jobPosition_id }: OpeningCreationAttributes = req.body;
 
-    const jobPosition = await JobPosition.findByPk(owner_jobPosition_id);
-    if (!jobPosition) {
-        res.json({
-            status: "error",
-            message: "Job position of Opening not found",
-        });
-        return;
+  if (!status || !reason_current_status || !open_date || !close_date || !close_reason || !hours_required || !owner_jobPosition_id) {
+    return res.status(400).json({
+      status: "error",
+      message: "All fields are required",
+    });
+  }
+
+  if (isNaN(owner_jobPosition_id)) {
+    return res.status(400).json({
+      status: "error",
+      message: "owner_jobPosition_id must be a valid number",
+    });
+  }
+
+  const jobPosition = await JobPosition.findByPk(owner_jobPosition_id);
+  if (!jobPosition) {
+    res.json({
+      status: "error",
+      message: "Job position of Opening not found",
+    });
+    return;
+  }
+
+  await Opening.create({ status, open_date, close_date, close_reason, hours_required, owner_jobPosition_id, reason_current_status }, { include: [{ model: JobPosition, as: "owner_jobPosition" }] }).then(
+    async (opening) => {
+      const openingWithAssociations = await Opening.findByPk(opening.id, { include: [{ model: JobPosition, as: "owner_jobPosition" }] });
+      res.json({
+        status: "success",
+        message: "Opening created",
+        data: openingWithAssociations,
+      });
     }
-    
-    await Opening.create({ status, open_date, close_date, close_reason, hours_required, owner_jobPosition_id, reason_current_status }, {include: [ { model: JobPosition, as: "owner_jobPosition" }] }).then(
-        async(opening) => {
-            const openingWithAssociations = await Opening.findByPk(opening.id, {include: [{ model: JobPosition, as: "owner_jobPosition" }]});
-            res.json({
-                status: "success",
-                message: "Opening created",
-                data: openingWithAssociations,
-            });
-        }
-    ).catch(
-        e => {
-            res.json({
-                status: "error",
-                message: "Opening not created",
-                error: e
-            });
-        }
-    );
+  ).catch(
+    e => {
+      res.json({
+        status: "error",
+        message: "Opening not created",
+        error: e
+      });
+    }
+  );
 }
 
 
 // Updating a opening
-export const editOpening = async (req: Request, res: Response) => {
+export const updateOpening = async (req: Request, res: Response) => {
   const { id } = req.params;
-  const {...resto } = req.body;  
+  const { ...resto } = req.body;
 
-//   dont update jobPosition_id
-//   delete resto.jobPosition_id;
+  const openingIdNumber = Number(id);
 
+  if (isNaN(openingIdNumber)) {
+    return res.status(400).json({
+      status: "error",
+      message: "Opening id must be valid a number",
+    });
+  }
 
-  await Opening.update(resto , { where: { id } })
+  await Opening.update(resto, { where: { id } })
     .then(async () => {
-      const openingUpdated = await Opening.findByPk(id, { include: [{ model: JobPosition, as: "owner_jobPosition" }] });
+      const updatedOpening = await Opening.findByPk(id, { include: [{ model: JobPosition, as: "owner_jobPosition" }] });
+      if (!updatedOpening) {
+        return res.status(404).json({
+          status: "error",
+          message: "Client not found",
+        });
+      }
       res.json({
         status: "success",
         message: "Opening updated",
-        data: openingUpdated,
+        data: updatedOpening,
       });
     })
     .catch((e) => {
@@ -123,6 +137,15 @@ export const editOpening = async (req: Request, res: Response) => {
 // Deleting a opening (soft delete)
 export const deleteOpening = async (req: Request, res: Response) => {
   const { id } = req.params;
+
+  const openingIdNumber = parseInt(id);
+
+  if (!openingIdNumber || isNaN(openingIdNumber)) {
+    return res.status(400).json({
+      status: "error",
+      message: "Opening id must be valid a number",
+    });
+  }
 
   await Opening.update({ activeDB: false }, { where: { id } })
     .then(() => {
